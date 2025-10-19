7cbd338b4257aaf6fa70c5f1cac37ded5c8128f2
ze0s <ze0s@riseup.net>
2025-10-19 16:08:47 +0200

    build: update goreleaser wix file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"os"
 	"runtime/debug"
@@ -9,15 +10,42 @@ import (
 	"github.com/autobrr/crossplatform-playground/application"
 )
 
+var (
+	version = "v0.0.0-dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "unknown"
+)
+
+const usage = `
+Usage: autobrr <action> [options]
+
+Actions:
+  version  Print version information
+`
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprint(flag.CommandLine.Output(), usage)
+	}
+}
+
 func main() {
-	fmt.Println("Hello World")
-	app := application.NewApplication()
-	ctx := context.Background()
+	flag.Parse()
 
 	defer logPanic()
 
-	if err := app.Start(ctx); err != nil {
-		os.Exit(1)
+	switch cmd := flag.Arg(0); cmd {
+	case "version":
+		fmt.Printf("Version: %s\nCommit: %s\nDate: %s\nBuilt by: %s\n", version, commit, date, builtBy)
+
+	default:
+		app := application.NewApplication()
+		ctx := context.Background()
+
+		if err := app.Start(ctx); err != nil {
+			os.Exit(1)
+		}
 	}
 }
 
